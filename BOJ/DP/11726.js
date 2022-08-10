28f1c45badb80734e623d9919a60dea2245036f8
Chriso004 <qudcksrlf1@gmail.com>
2022-08-10 09:44:48 +0900

    11726 문제 정보 추가

--- BOJ/DP/11726.js
+++ BOJ/DP/11726.js
@@ -1,3 +1,8 @@
+/* 
+    https://www.acmicpc.net/problem/11726
+    2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+*/
+
 const fs = require('fs');
 const input = fs.readFileSync('test.txt').toString().split('\n'); // 테스트용 파일
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
